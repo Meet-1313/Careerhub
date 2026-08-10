8a6522a2b03c9b7ec39cfa6161e60242d1df6fe4
Meet-1313 <solankimeet@1779gmail.com>
2026-08-11 00:21:25 +0530

    Fix pagination import

--- frontend/src/components/shared/Pagination.jsx
+++ frontend/src/components/shared/Pagination.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/Button";
+import { Button } from "@/components/ui/button";
 
 function Pagination ({ currentPage, totalPages, onPageChange}) {
     if (totalPages <= 1) {
